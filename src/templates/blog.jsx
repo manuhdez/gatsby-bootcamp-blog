e6c3affbe94169ade22257f69f152cdc851022b2
Manu Hdez <manu.hdez.dev@gmail.com>
2019-05-19 10:21:02 +0100

    #14 showing content from markdown on templates

--- src/templates/blog.jsx
+++ src/templates/blog.jsx
@@ -1,9 +1,29 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout/layout"
 
-const blog = () => {
-  return <Layout>This is the blog template</Layout>
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+      }
+      html
+    }
+  }
+`
+
+const blog = props => {
+  const { frontmatter, html } = props.data.markdownRemark
+  return (
+    <Layout>
+      <h1>{frontmatter.title}</h1>
+      <p>{frontmatter.date}</p>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </Layout>
+  )
 }
 
 export default blog
